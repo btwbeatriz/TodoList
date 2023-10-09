ca32052a9aff4896346300fe646da4feab298d42
btwbeatriz <beatriz.ffreitas@hotmail.com>
2023-10-09 10:07:20 -0300

    Initial commit

---
+++ src/main/java/br/com/rocketseat/todolist/controller/PrimeiraController.java
@@ -0,0 +1,15 @@
+package br.com.rocketseat.todolist.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiraRota")
+public class PrimeiraController {
+
+    @GetMapping("/primeiroMetodo")
+    public String primeiraMensagem(){
+        return "Funcionou";
+    }
+}
