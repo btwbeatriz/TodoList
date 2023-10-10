7df1f7f59f0b6d1e2d2197a61820ff540297d4e8
btwbeatriz <beatriz.ffreitas@hotmail.com>
2023-10-10 09:21:30 -0300

    validando o usuário

--- src/main/java/br/com/rocketseat/todolist/user/IUserRepository.java
+++ src/main/java/br/com/rocketseat/todolist/user/IUserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
 }
