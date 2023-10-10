395b03c777f934992979d132e2cab0036fcf9062
btwbeatriz <beatriz.ffreitas@hotmail.com>
2023-10-10 08:36:13 -0300

    implementação do lombok

--- src/main/java/br/com/rocketseat/todolist/user/UserModel.java
+++ src/main/java/br/com/rocketseat/todolist/user/UserModel.java
@@ -1,6 +1,10 @@
 package br.com.rocketseat.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
+
     private String username;
     private String name;
     private String password;
