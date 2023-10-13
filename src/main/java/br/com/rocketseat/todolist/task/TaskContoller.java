0ec575bea993754007627c4ccbdb1b9457bbbd9e
btwbeatriz <beatriz.ffreitas@hotmail.com>
2023-10-13 13:34:50 -0300

    Criando updates parciais

--- src/main/java/br/com/rocketseat/todolist/task/TaskContoller.java
+++ src/main/java/br/com/rocketseat/todolist/task/TaskContoller.java
@@ -1,5 +1,6 @@
 package br.com.rocketseat.todolist.task;
 
+import br.com.rocketseat.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -45,9 +46,11 @@ public class TaskContoller {
 
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
-        var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 }
